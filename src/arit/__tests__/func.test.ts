b39f62e09b11c53c8ccbcffccb6778f4d562a6cf
godavdev <dgaduff@gmail.com>
2026-04-20 12:39:40 -0600

    Add subtraction and multiplication tests; update import statements

--- src/arit/__tests__/func.test.ts
+++ src/arit/__tests__/func.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test"
-import { divide, sum } from "../func"
+import { divide, sum, subtract, multiply } from "../func"
 
 describe("func", () => {
   test("2 + 6 sum should return 8", () => {
@@ -8,6 +8,26 @@ describe("func", () => {
 
   test("-2 + 6 sum should return 4", () => {
     expect(sum(-2, 6)).toBe(4)
+  })
+
+  test("10 - 4 subtract should return 6", () => {
+    expect(subtract(10, 4)).toBe(6)
+  })
+
+  test("-5 - 5 subtract should return -10", () => {
+    expect(subtract(-5, 5)).toBe(-10)
+  })
+
+  test("3 * 4 multiply should return 12", () => {
+    expect(multiply(3, 4)).toBe(12)
+  })
+
+  test("-2 * 5 multiply should return -10", () => {
+    expect(multiply(-2, 5)).toBe(-10)
+  })
+
+  test("10 / 2 divide should return 5", () => {
+    expect(divide(10, 2)).toBe(5)
   })
 
   test("diving 2 by 0 should throw an error", () => {
